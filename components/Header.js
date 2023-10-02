c41266863ee392c54f8a3de1518b2a54347abd47
Dipesh chauhan <dpshchhn@gmail.com>
2023-10-02 10:36:01 +0545

    social medias

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-center items-center gap-y-6 py-8">
+        <div className="flex flex-col lg:flex-row lg:justify-between justify-center items-center gap-y-6 py-8">
           <Link href={"/"}>
             <Image
               src={"/logo.svg"}
@@ -16,6 +16,7 @@ const Header = () => {
               priority={true}
             />
           </Link>
+          <Socials />
         </div>
       </div>
     </header>
